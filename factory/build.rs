d689636cf4321c06d57e5965cb597cf83235f0c3
dj8yf0μl <feepdake27@gmail.com>
2024-05-17 00:57:02 +0300

    change to revision of `cargo-near` with cmd mini-parser

--- factory/build.rs
+++ factory/build.rs
@@ -1,12 +1,8 @@
-
 fn main() {
-    let build_opts = cargo_near::BuildOpts::new(
-        false, false, false, false, false, None, None, None, false, None,
-    );
     let opts = cargo_near::build_rs::SubBuildOpts {
         workdir: "../product-donation",
         metadata_contract_path: "product-donation",
-        build_command: ("cargo near build".to_string(), build_opts),
+        build_command: "cargo near build".to_string(),
 
         distinct_target_dir: "../target/sub-build-product-donation",
         skipped_profiles: vec!["debug"],
@@ -16,6 +12,3 @@ fn main() {
     let result_env_key = "BUILD_RS_SUB_BUILD_ARTIFACT_1".to_string();
     cargo_near::build_rs::process_sub_build(opts, result_env_key).expect("sub build error");
 }
-
-
-
