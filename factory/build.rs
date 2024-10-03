a3334edbed75fea89fc4cda4047db7ad37b9a3ea
dj8yf0μl <noreply@nowhere.org>
2024-10-03 17:46:05 +0300

    update build-script to latest from pr 228

--- factory/build.rs
+++ factory/build.rs
@@ -1,37 +1,42 @@
-use cargo_near_build::{extended::BuildScriptOpts, BuildImplicitEnvOpts};
+use cargo_near_build::{bon, extended};
+use cargo_near_build::{BuildImplicitEnvOpts, BuildOpts};
 
 fn main() {
     let _e = env_logger::Builder::new().parse_default_env().try_init();
 
-    let regular_build_opts = cargo_near_build::BuildOpts {
-        no_default_features: true,
-        ..Default::default()
-    };
+    // directory of target sub-contract's crate
+    let workdir = "../product-donation";
+    // unix path to target sub-contract's crate from root of the repo
+    let nep330_contract_path = "product-donation";
 
-    let opts = cargo_near_build::extended::BuildOptsExtended {
-        workdir: "../product-donation".into(),
-        build_opts: regular_build_opts,
-        build_implicit_env_opts: BuildImplicitEnvOpts {
-            nep330_contract_path: Some("product-donation".into()),
-            cargo_target_dir: Some("../target/build-rs-product-donation".into()),
-        },
-        build_script_opts: BuildScriptOpts {
-            result_env_key: Some("BUILD_RS_SUB_BUILD_ARTIFACT_1".into()),
-            rerun_if_changed_list: vec![
-                "../product-donation".into(),
-                "../Cargo.toml".into(),
-                "../Cargo.lock".into(),
-            ],
-            build_skipped_when_env_is: vec![
-                // shorter build for `cargo check`
-                ("PROFILE".into(), "debug".into()),
-                (
-                    cargo_near_build::env_keys::BUILD_RS_ABI_STEP_HINT.into(),
-                    "true".into(),
-                ),
-            ],
-            stub_path: Some("../target/stub.bin".into()),
-        },
-    };
-    cargo_near_build::extended::build(opts).expect("sub-contract build error");
+    let build_opts = BuildOpts::builder().no_default_features(true).build();
+
+    let pwd = std::env::current_dir().expect("get pwd");
+    let distinct_target = pwd.join("../target/build-rs-product-donation");
+    let stub_path = pwd.join("../target/stub.bin");
+
+    let build_implicit_env_opts = BuildImplicitEnvOpts::builder()
+        .nep330_contract_path(nep330_contract_path)
+        .cargo_target_dir(distinct_target.to_string_lossy())
+        .build();
+
+    let build_script_opts = extended::BuildScriptOpts::builder()
+        .rerun_if_changed_list(bon::vec![workdir, "../Cargo.toml", "../Cargo.lock",])
+        .build_skipped_when_env_is(vec![
+            // shorter build for `cargo check`
+            ("PROFILE", "debug"),
+            (cargo_near_build::env_keys::BUILD_RS_ABI_STEP_HINT, "true"),
+        ])
+        .stub_path(stub_path.to_string_lossy())
+        .result_env_key("BUILD_RS_SUB_BUILD_ARTIFACT_1")
+        .build();
+
+    let extended_opts = extended::BuildOptsExtended::builder()
+        .workdir(workdir)
+        .build_opts(build_opts)
+        .build_implicit_env_opts(build_implicit_env_opts)
+        .build_script_opts(build_script_opts)
+        .build();
+
+    cargo_near_build::extended::build(extended_opts).expect("sub-contract build error");
 }
