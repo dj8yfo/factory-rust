e0b72085b625458e4d3d66f7421c350ab9979168
dj8yf0μl <noreply@nowhere.org>
2024-10-11 22:06:55 +0300

    add docker metadata test

--- factory/tests/sandbox.rs
+++ factory/tests/sandbox.rs
@@ -6,14 +6,14 @@ use serde_json::json;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _e = env_logger::try_init();
     let sandbox = near_workspaces::sandbox().await?;
-    let artifact = cargo_near_build::build(BuildOpts::default())?;
+    let artifact = cargo_near_build::build(BuildOpts::default(), None)?;
 
     let contract_wasm = std::fs::read(artifact.path)?;
     let contract = sandbox.dev_deploy(&contract_wasm).await?;
 
     let alice = sandbox
         .create_tla(
-            "alice.test.near".parse().unwrap(),
+            "alice".parse().unwrap(),
             SecretKey::from_random(KeyType::ED25519),
         )
         .await?
@@ -56,61 +56,54 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[tokio::test]
-async fn test_too_low_deposit() -> Result<(), Box<dyn std::error::Error>> {
+async fn docker_test_meta() -> Result<(), Box<dyn std::error::Error>> {
     let _e = env_logger::try_init();
     let sandbox = near_workspaces::sandbox().await?;
-    let artifact = cargo_near_build::build(BuildOpts::default())?;
+    let artifact = cargo_near_build::docker::build(Default::default())?;
 
     let contract_wasm = std::fs::read(artifact.path)?;
     let contract = sandbox.dev_deploy(&contract_wasm).await?;
 
     let alice = sandbox
         .create_tla(
-            "alice.test.near".parse().unwrap(),
+            "alice".parse().unwrap(),
             SecretKey::from_random(KeyType::ED25519),
         )
         .await?
         .unwrap();
 
+    let bob = sandbox.dev_create_account().await?;
+
     let res = contract
         .call("create_factory_subaccount_and_deploy")
         .args_json(json!({"name": "donation_for_alice", "beneficiary": alice.id()}))
         .max_gas()
-        .deposit(NearToken::from_near(1)) // NOTE: this is less than 1.55 NEAR
+        .deposit(NearToken::from_near(5))
         .transact()
         .await?;
 
-    // 1.55 NEAR corresponds to size of donation contract, deployed by factory
-    // Storage used by the account      154.9 KB
-    // assert!(format!("{:?}", res.into_result().unwrap_err()).contains("Attach at least 1.55 NEAR"));
-    // TODO: replace all of below with above line
+    assert!(res.is_success());
 
-    let res = res.into_result();
-    match res {
-        Err(err) => {
-            // noop
-            println!("we've hit the expected assert branch");
-            assert!(format!("{:?}", err).contains("Attach at least 1."));
-        }
-        Ok(_value) => {
-            // NOTE: this branch is hit, if
-            // https://github.com/near-examples/factory-rust/blob/main/src/lib.rs#L8
-            // is reverted to NearToken::from_yoctonear(10u128.pow(18))
-            let bob = sandbox.dev_create_account().await?;
-            let sub_accountid: AccountId = format!("donation_for_alice.{}", contract.id())
-                .parse()
-                .unwrap();
-            let view_res = bob
-                .view(&sub_accountid, "get_beneficiary")
-                .args_json({})
-                .await;
-            assert!(view_res.is_err()); // NOTE: this line becomes `is_ok()` if deposit is increased `1` -> `2 NEAR`
-            let dbg = format!("{:#?}", view_res.unwrap_err());
+    let sub_accountid: AccountId = format!("donation_for_alice.{}", contract.id())
+        .parse()
+        .unwrap();
 
-            // NOTE: donation contract wasn't deployed
-            assert!(dbg.contains("donation_for_alice"));
-            assert!(dbg.contains("UnknownAccount"));
-        }
-    }
+    let res = bob
+        .view(&sub_accountid, "get_beneficiary")
+        .args_json({})
+        .await?;
+
+    assert_eq!(res.json::<AccountId>()?, alice.id().clone());
+
+    let res = bob
+        .call(&sub_accountid, "donate")
+        .args_json({})
+        .max_gas()
+        .deposit(NearToken::from_near(5))
+        .transact()
+        .await?;
+
+    assert!(res.is_success());
+
     Ok(())
 }
