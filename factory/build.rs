617a93075cd16497cc8e3f0ebbf3089e70ea0e5c
dj8yf0μl <noreply@nowhere.org>
2024-10-11 21:58:50 +0300

    update rev to 1cfb from pr 228; same docker image

--- factory/build.rs
+++ factory/build.rs
@@ -1,4 +1,6 @@
-use cargo_near_build::{bon, extended};
+use std::str::FromStr;
+
+use cargo_near_build::{bon, camino, extended};
 use cargo_near_build::{BuildImplicitEnvOpts, BuildOpts};
 
 fn main() {
@@ -9,16 +11,18 @@ fn main() {
     // unix path to target sub-contract's crate from root of the repo
     let nep330_contract_path = "product-donation";
 
-    let build_opts = BuildOpts::builder().no_default_features(true).build();
-
-    let pwd = std::env::current_dir().expect("get pwd");
-    // a distinct target is needed to avoid deadlock during build
-    let distinct_target = pwd.join("../target/build-rs-product-donation");
-    let stub_path = pwd.join("../target/stub.bin");
+    let manifest = camino::Utf8PathBuf::from_str(workdir)
+        .expect("pathbuf from str")
+        .join("Cargo.toml");
+    let build_opts = BuildOpts::builder()
+        .no_default_features(true)
+        .manifest_path(manifest)
+        .build();
 
     let build_implicit_env_opts = BuildImplicitEnvOpts::builder()
         .nep330_contract_path(nep330_contract_path)
-        .cargo_target_dir(distinct_target.to_string_lossy())
+        // a distinct target is needed to avoid deadlock during build
+        .cargo_target_dir("../target/build-rs-product-donation")
         .build();
 
     let build_script_opts = extended::BuildScriptOpts::builder()
@@ -28,12 +32,11 @@ fn main() {
             ("PROFILE", "debug"),
             (cargo_near_build::env_keys::BUILD_RS_ABI_STEP_HINT, "true"),
         ])
-        .stub_path(stub_path.to_string_lossy())
+        .stub_path("../target/stub.bin")
         .result_env_key("BUILD_RS_SUB_BUILD_ARTIFACT_1")
         .build();
 
     let extended_opts = extended::BuildOptsExtended::builder()
-        .workdir(workdir)
         .build_opts(build_opts)
         .build_implicit_env_opts(build_implicit_env_opts)
         .build_script_opts(build_script_opts)
